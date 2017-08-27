ac7e05ea593995d1823e06f67836fd68944b8b17
Jake Zheng <momoko8443@163.com>
2017-08-27 17:26:53 +0800

    update

--- lib/SegmentFault.ts
+++ lib/SegmentFault.ts
@@ -8,26 +8,28 @@ export let SegmentFault = class SegmentFault {
     private viewViewModelMap = {};
     private renderer = new Renderer();
     private generator:ComponentGenerator;
-    public init() {
+    public init():Promise<any>{
         //web component相关功能的扩展 从此行开始
         //Hi Scanner,Watch你们俩先别急着监视，扫描,先让我把Componont的定义load进来。
-
-        let loader = new Loader(this.componentPool);
-        loader.load().then( componentDefinitionPool =>{
-            console.log(componentDefinitionPool);
-
-            this.generator = new ComponentGenerator(this,componentDefinitionPool);
-            return this.generator.scanComponent(document);
-        }).then(()=>{
-            let scanner = new Scanner(this.viewModelPool);
-            let watcher = new Watcher(this);
-            for (let key in this.viewModelPool) {
-                watcher.observe(this.viewModelPool[key],this.viewModelChangedHandler);
-            }
-            this.viewViewModelMap = scanner.scanBindDOM();
-            Object.keys(this.viewViewModelMap).forEach(alias=>{
-                this.refresh(alias);
-            }); 
+        return new Promise((resolve,reject)=>{
+            let loader = new Loader(this.componentPool);
+            loader.load().then( componentDefinitionPool =>{
+                console.log(componentDefinitionPool);
+    
+                this.generator = new ComponentGenerator(this,componentDefinitionPool);
+                return this.generator.scanComponent(document);
+            }).then(()=>{
+                let scanner = new Scanner(this.viewModelPool);
+                let watcher = new Watcher(this);
+                for (let key in this.viewModelPool) {
+                    watcher.observe(this.viewModelPool[key],this.viewModelChangedHandler);
+                }
+                this.viewViewModelMap = scanner.scanBindDOM();
+                Object.keys(this.viewViewModelMap).forEach(alias=>{
+                    this.refresh(alias);
+                }); 
+                resolve();
+            });
         });
     };
     public registerViewModel(alias:string, viewModel:object) {
